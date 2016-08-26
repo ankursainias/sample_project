14910c9ddd801aa695193cdce8f3d13188a32dd1
Tushar Bhatia <tallent16@netsetsoftwware.com>
2016-08-26 18:58:57 +0530

    remove validation for test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -31,7 +31,7 @@ before_action :verified_user  , except: [:sign_up,:sign_in, :update ,:check_otp,
 				if user.valid_password?(params[:user][:password])
 					user.create_token if user.api_secret.blank?
 					@user=user
-					raise "phone_number_not_verified" unless @user.verified
+					# raise "phone_number_not_verified" unless @user.verified
 					@current_api_user=user
 					render :me
 				else 
